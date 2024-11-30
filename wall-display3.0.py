7f30257c1bfee12c4791b1d4a1e9b1d2efde967c
Agnaldo Silva Lima <a.agnaldosilva@gmail.com>
2024-11-29 21:24:18 -0300

    update

--- wall-display3.0.py
+++ wall-display3.0.py
@@ -1,6 +1,4 @@
 # ***************************************************************************
-# *   Copyright (C) 2014 by Edson Borin                                     *
-# *   edson@ic.unicamp.br                                                   *
 # *                                                                         *
 # *   This program is free software; you can redistribute it and/or modify  *
 # *   it under the terms of the GNU General Public License as published by  *
