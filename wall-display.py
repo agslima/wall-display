fbdbbf6b964eed11a1038e16f27579fb76473b7d
Agnaldo Silva Lima <a.agnaldosilva@gmail.com>
2025-08-22 23:14:55 -0300

    Update wall-display.py

--- wall-display.py
+++ wall-display.py
@@ -10,10 +10,9 @@
 # * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
 # * GNU General Public License for more details.                          *
 # * *
-# * You should have received a copy of the GNU General Public License     *
-# * along with this program; if not, write to the                         *
-# * Free Software Foundation, Inc.,                                       *
-# * 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
+# * You should have received a copy of the GNU General Public License.    *
+# *	along with this program. If not, see <http://www.gnu.org/licenses/>.  *
+# **
 # ***************************************************************************
 
 import pygame
@@ -290,4 +289,5 @@ if __name__ == "__main__":
     # Or, run with the default directory
     app = WallDisplayApp()
     app.run()
+
 
