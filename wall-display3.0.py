c934b6ba96b94898632a11d443a71cb080204fe2
Agnaldo Silva Lima <a.agnaldosilva@gmail.com>
2025-08-22 23:16:16 -0300

    Update wall-display3.0.py

--- wall-display3.0.py
+++ wall-display3.0.py
@@ -15,7 +15,7 @@
 # *   Free Software Foundation, Inc.,                                       *
 # *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
 # ***************************************************************************
-
+"""
 import pygame
 import sys
 import os
@@ -265,3 +265,5 @@ while not done:
         #    print "Key pressed: "+str(event.key)
             
 pygame.quit();
+
+"""
